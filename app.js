720234d38e29ebed9b9b1296cac495f37879c762
Sanmay <bhavanish2000@gmail.com>
2022-03-02 22:01:13 +0530

    version 125

--- app.js
+++ app.js
@@ -1375,6 +1375,11 @@ app.post("/chat", upload.single('image'), function(req, res) {
 
 /////////////////////////////listen///////////////////////////////
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("in port 3000");
 });
